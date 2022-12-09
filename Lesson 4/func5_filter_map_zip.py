edf4f209815907b4a25e6805c52905f87af0857b
amirjj <amir.jamshidijam@gmail.com>
2022-12-09 23:55:46 +0330

    minor change

--- Lesson 4/func5_filter_map_zip.py
+++ Lesson 4/func5_filter_map_zip.py
@@ -14,4 +14,16 @@ list(filter(None, a))
 b = [True, False, True, True]
 list(filter(None, b))
 
-
+"""
+one usage of lambda is to define a logic for sort like:
+"""
+my_dict = [
+    {'name' : 'john', 'score' : 3},
+    {'name' : 'rex', 'score' : 1},
+    {'name' : 'alice', 'score' : 2},
+    {'name' : 'jake', 'score' : 4},
+]
+sorted_dict = sorted(my_dict, key=lambda x: x['score'])
+print(sorted_dict)
+my_dict.sort(key= lambda x: x['score'])
+print(my_dict)
