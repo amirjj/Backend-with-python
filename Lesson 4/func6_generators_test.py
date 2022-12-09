df72d834603bbe75c16492f8b6fa5494e51846d1
amirjj <amir.jamshidijam@gmail.com>
2022-12-09 22:24:23 +0330

    generator vs regular

--- Lesson 4/func6_generators_test.py
+++ Lesson 4/func6_generators_test.py
@@ -2,33 +2,33 @@ from time import time
 
 first_ts = time()
 
-def regular_file_reader(filename):
-    file_ = open(filename, "r")
-    data = file_.readlines()
-    file_.close()
-    return data
+# def regular_file_reader(filename):
+#     file_ = open(filename, "r")
+#     data = file_.readlines()
+#     file_.close()
+#     return data
 
-for row in regular_file_reader("sample_file.csv"):
-    continue
-    # print(row)
-    # global second_time
-    # second_time = time()
-    # break
-
-
-
-# def generator_file_reader(filename):
-#     with open(filename, "r") as f:
-#         for row in f:
-#             yield row
-
-
-# for row in generator_file_reader("sample_file.csv"):
+# for row in regular_file_reader("sample_file.csv"):
 #     continue
 #     # print(row)
 #     # global second_time
 #     # second_time = time()
 #     # break
+
+
+
+def generator_file_reader(filename):
+    with open(filename, "r") as f:
+        for row in f:
+            yield row
+
+
+for row in generator_file_reader("sample_file.csv"):
+    print(row)
+    global second_time
+    second_time = time()
+    break
+    continue
 
 second_time = time()
 print(second_time - first_ts)
@@ -39,3 +39,10 @@ regular reading a file (reading the whole file first) improved significantly.
 does this fact still correct if you want to read all file? No
 """
 
+
+"""
+While both return and yield statements are performing similarly on the time front, 
+it is in the area of memory consumption where the generators really outshine the return statement.
+
+https://www.pylenin.com/blogs/how-fast-are-generators/
+"""
