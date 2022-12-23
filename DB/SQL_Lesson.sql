68a9ab5232f361fac858f5b817b7604f83a785c8
amirjj <amir.jamshidijam@gmail.com>
2022-12-23 12:39:26 +0330

    SQL Notes added

---
+++ DB/SQL_Lesson.sql
@@ -0,0 +1,18 @@
+--Which one of below Queries are right?
+
+--Number 1
+SELECT CustomerID, count(*) as order_count FROM [Orders] WHERE order_count > 5
+GROUP BY CustomerID
+ORDER BY order_count desc
+
+
+--Number 2
+SELECT CustomerID, count(*) AS order_count FROM [Orders]
+GROUP BY CustomerID HAVING order_count > 5
+ORDER BY order_count DESC
+
+--Note: When using aggregate functions (COUNT, SUM, MAX, ...) you cant just use
+--WHERE after SELECT, because the aggregate function is not processed until
+--GROUP BY has done, so you have to use HAVING operator in aggregate functions
+--for implementing conditions
+--Number 2 is correct
