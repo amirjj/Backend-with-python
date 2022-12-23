ec48d6d33e8f173231158337a2cf709758307724
amirjj <amir.jamshidijam@gmail.com>
2022-12-23 15:20:25 +0330

    renamed

--- DB/SQL_Lesson.sql
+++ DB/SQL_Lesson.sql
@@ -7,12 +7,17 @@ ORDER BY order_count desc
 
 
 --Number 2
-SELECT CustomerID, count(*) AS order_count FROM [Orders]
+SELECT CustomerID, count(*) AS order_count FROM Orders
 GROUP BY CustomerID HAVING order_count > 5
 ORDER BY order_count DESC
 
---Note: When using aggregate functions (COUNT, SUM, MAX, ...) you cant just use
+--Note: If the condition is on aggregation function use HAVING
+--When using aggregate functions (COUNT, SUM, MAX, ...) you cant just use
 --WHERE after SELECT, because the aggregate function is not processed until
 --GROUP BY has done, so you have to use HAVING operator in aggregate functions
 --for implementing conditions
 --Number 2 is correct
+
+SELECT CustomerID, count(*) AS order_count FROM Orders WHERE Date > '1990-01-01'
+GROUP BY CustomerID HAVING order_count > 5
+ORDER BY order_count DESC
--- Decorators/dec_1.py
+++ Decorators/dec_1.py
@@ -19,4 +19,3 @@ def pow2(num):
 
 if __name__ == "__main__":
     print(pow2(10))
-
