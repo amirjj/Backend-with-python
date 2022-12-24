fe3d0386e102b4bb9e796ac318523bd10e0fe98b
amirjj <amir.jamshidijam@gmail.com>
2022-12-24 17:47:17 +0330

    comment added

--- Others/char_encodings.py
+++ Others/char_encodings.py
@@ -4,6 +4,20 @@ December 2007, when UTF-8 encoding surpassed it; UTF-8 is backward
 compatible with ASCII
 """
 
+"""
+URL encoding converts characters into a format that can be transmitted over the 
+Internet.
+
+URLs can only be sent over the Internet using the ASCII character-set.
+Since URLs often contain characters outside the ASCII set, the URL has to be 
+converted into a valid ASCII format.
+URL encoding replaces unsafe ASCII characters with a "%" followed by two 
+hexadecimal digits.
+URLs cannot contain spaces. URL encoding normally replaces a space with a 
+plus (+) sign or with %20.
+
+
+"""
 
 def print_asciis():
     for i in range(2000):
